e0139827d3faf810e6a4971d3fd99031462418ab
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-05-31 19:52:43 +0200

    Add Board and Consts

---
+++ Battleships.Core/Models/Board.cs
@@ -0,0 +1,11 @@
+﻿using Battleships.Core.Utils;
+
+namespace Battleships.Core.Models
+{
+    public class Board
+    {
+        private readonly FieldState[,] _innerBoard = new FieldState[Consts.Rows, Consts.Columns];
+        
+        public FieldState this[int row, int column] => _innerBoard[row, column];
+    }
+}
---
+++ Battleships.Core/Utils/Consts.cs
@@ -0,0 +1,9 @@
+﻿namespace Battleships.Core.Utils
+{
+    public static class Consts
+    {
+        public static int Columns = 10;
+        public static int Rows = 10;
+
+    }
+}
