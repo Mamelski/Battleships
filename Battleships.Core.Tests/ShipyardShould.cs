d8e960244146ed2d0b0c447d8d1e23a3a0942676
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-06-01 20:02:11 +0200

    Add shipyard tests

---
+++ Battleships.Core.Tests/ShipyardShould.cs
@@ -0,0 +1,16 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace Battleships.Core.Tests
+{
+    public class ShipyardShould
+    {
+        [Fact]
+        public void ProduceSomeShips()
+        {
+            var ships = Shipyard.ProduceShips();
+            
+            ships.ShouldNotBeEmpty();
+        }
+    }
+}
