015588d405ffe2d431a97184c26cadd62876d837
Jakub Mamelski <jakub.mamelski@outlook.com>
2021-06-02 10:17:39 +0200

    Introduce ConsolePrinter to handle printing :)

---
+++ Battleships.Cmd/ConsolePrinter.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Battleships.Core.Models;
+using Battleships.Core.Utils;
+
+namespace Battleships.Cmd
+{
+    public static class ConsolePrinter
+    {
+        public static void PrintIntroduction(Board board)
+        {
+            Console.WriteLine();
+            PrintInColor(ConsoleColor.Yellow, "Hello Commander! ");
+            Console.Write("Enemies are at our waters and we need to ");
+            PrintInColor(ConsoleColor.DarkRed, "destroy them!");
+            Console.WriteLine();
+
+            Console.WriteLine("I am the next generation AI system and I will help you win this battle.");
+            Console.WriteLine("I have intel what ships our enemy has:");
+            Console.WriteLine();
+            Console.WriteLine("- 1x Battleship (5 squares)");
+            Console.WriteLine("- 2x Destroyers (4 squares)");
+            Console.WriteLine();
+            Console.WriteLine("Unfortunately I don't know their exact location, so we need to shoot blindly and hope for a hit.");
+
+            Console.WriteLine("Battlefield is a 10x10 grid.");
+            Console.WriteLine("Every coordinate is identified by a letter (\"a\" to \"j\") and a number (\"1\" to \"10\"):");
+            Console.WriteLine();
+            PrintBoard(board);
+            PrintLegend();
+
+            Console.WriteLine("Now you need to decide how we play this battle:");
+            Console.WriteLine();
+            Console.Write("- type ");
+            PrintInColor(ConsoleColor.Green, $"{GameMode.simulation}");
+            Console.WriteLine(" to let me win this battle for us");
+            Console.Write("- type ");
+            PrintInColor(ConsoleColor.Green, $"{GameMode.manual}");
+            Console.WriteLine(" to fight without my help (I will still show you state of the battlefield)");
+            Console.WriteLine();
+        }
+
+        public static void PrintBoard(Board board)
+        {
+            Console.WriteLine("    A B C D E F G H I J ");
+            Console.WriteLine("    --------------------");
+
+            for (var row = 0; row < Consts.Rows; row++)
+            {
+                Console.Write(row < 9 ? $"{row + 1}  |" : $"{row + 1} |");
+
+                for (var column = 0; column < Consts.Columns; column++)
+                {
+                    PrintCoordinateState(board[row, column]);
+                }
+                Console.Write("|\n");
+            }
+            Console.WriteLine("    --------------------");
+        }
+        
+        private static void PrintLegend()
+        {
+            Console.WriteLine("Coordinates can have few possible states:");
+            Console.WriteLine();
+            
+            PrintCoordinateState(CoordinateState.Unknown);
+            Console.WriteLine(" - means that have no idea what is there");
+            
+            PrintCoordinateState(CoordinateState.Miss);
+            Console.WriteLine(" - means that we shot there and missed");
+            
+            PrintCoordinateState(CoordinateState.Hit);
+            Console.WriteLine(" - means that we hit part of the enemy ship");
+            
+            PrintCoordinateState(CoordinateState.Sunk);
+            Console.WriteLine(" - means that we hit enemy ship on this coordinate and sunk it");
+            
+            Console.WriteLine();
+        }
+        
+        private static void PrintCoordinateState(CoordinateState coordinateState)
+        {
+            switch (coordinateState)
+            {
+                case CoordinateState.Miss:
+                    PrintInColor(ConsoleColor.White, ". ");
+                    break;
+                case CoordinateState.Hit:
+                    PrintInColor(ConsoleColor.Yellow, "x ");
+                    break;
+                case CoordinateState.Sunk:
+                    PrintInColor(ConsoleColor.DarkRed, "x ");
+                    break;
+                case CoordinateState.Unknown:
+                    Console.Write("  ");
+                    break;
+            }
+        }
+
+        private static void PrintInColor(
+            ConsoleColor color,
+            string message)
+        {
+            Console.ForegroundColor = color;
+            Console.Write(message);
+            Console.ResetColor();
+        }
+    }
+}
--- Battleships.Cmd/Program.cs
+++ Battleships.Cmd/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using Battleships.Core;
 using Battleships.Core.Models;
@@ -14,54 +13,10 @@ namespace Battleships.Cmd
         static void Main(string[] args)
         {
             var gameManager = new GameManager(new RandomShipPlacer(), new BattleshipGameEngine());
-            
             var board = gameManager.StartGame();
             
-            Console.WriteLine();
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Hello Commander! ");
-            Console.ResetColor();
+            ConsolePrinter.PrintIntroduction(board);
 
-            Console.Write("Enemies are at our waters and we need to ");
-            Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.Write("destroy them!");
-            Console.ResetColor();
-            Console.WriteLine();
-
-            Console.WriteLine("I am the next generation AI system and I will help you win this battle.");
-            Console.WriteLine("I have intel what ships our enemy has:");
-            Console.WriteLine();
-            Console.WriteLine("- 1x Battleship (5 squares)");
-            Console.WriteLine("- 2x Destroyers (4 squares)");
-            Console.WriteLine();
-            
-            Console.WriteLine("Unfortunately I don't know their exact location, so we need to shoot blindly and hope for a hit.");
-
-            
-            Console.WriteLine("Battlefield is a 10x10 grid.");
-            Console.WriteLine("Every coordinate is identified by a letter (\"a\" to \"j\") and a number (\"1\" to \"10\"):");
-            Console.WriteLine();
-            PrintBoard(board);
-
-            PrintLegend();
-
-            Console.WriteLine("Now you need to decide how we play this battle:");
-            Console.WriteLine();
-
-            Console.Write("- type ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(GameMode.simulation);
-            Console.ResetColor();
-            Console.WriteLine(" to let me win this battle for us");
-            
-            Console.Write("- type ");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.Write(GameMode.manual);
-            Console.ResetColor();
-            Console.WriteLine(" to fight without my help (I will still show you state of the battlefield)");
-            Console.WriteLine();
-
-            var modeChosen = false;
             while (true)
             {
                 var mode = Console.ReadLine();
@@ -93,7 +48,7 @@ namespace Battleships.Cmd
                         var result = gameManager.Shoot(shotCoordinates);
                 
                         // var result = gameManager.Shoot(randomCoordinate);
-                        PrintBoard(result.Board);
+                       ConsolePrinter.PrintBoard(result.Board);
                         Thread.Sleep(100);
                     }
                     Console.WriteLine("MANUAL");
@@ -128,7 +83,7 @@ namespace Battleships.Cmd
                 allCoordinates.Remove(randomCoordinate);
 
                 var result = gameManager.Shoot(randomCoordinate);
-                PrintBoard(result.Board);
+                ConsolePrinter.PrintBoard(result.Board);
                 
                 if (result.IsGameFinished)
                 {
@@ -145,56 +100,7 @@ namespace Battleships.Cmd
             }
         }
 
-        private static void PrintLegend()
-        {
-            Console.WriteLine("Coordinates can have few possible states:");
-            Console.WriteLine();
-            
-            PrintCoordinateState(CoordinateState.Unknown);
-            Console.WriteLine(" - means that have no idea what is there");
-            
-            PrintCoordinateState(CoordinateState.Miss);
-            Console.WriteLine(" - means that we shot there and missed");
-            
-            PrintCoordinateState(CoordinateState.Hit);
-            Console.WriteLine(" - means that we hit part of the enemy ship");
-            
-            PrintCoordinateState(CoordinateState.Sunk);
-            Console.WriteLine(" - means that we hit enemy ship on this coordinate and sunk it");
-            
-            Console.WriteLine();
-        }
-
-        public static void PrintBoard(Board board)
-        {
-            var stringBuilder = new StringBuilder();
-
-            Console.WriteLine("    A B C D E F G H I J ");
-            Console.WriteLine("    --------------------");
-
-            for (var row = 0; row < Consts.Rows; row++)
-            {
-                if (row < 9)
-                {
-                    Console.Write($"{row + 1}  |");
-                }
-                else
-                {
-                    Console.Write($"{row + 1} |");
-                }
-
-                for (var column = 0; column < Consts.Columns; column++)
-                {
-                    PrintCoordinateState(board[row, column]);
-                }
-
-                Console.Write("|\n");
-            }
-            
-            stringBuilder.AppendLine("    --------------------");
-            
-            Console.WriteLine(stringBuilder);
-        }
+     
 
         private static Coordinate ParseMove(string move)
         {
@@ -216,35 +122,7 @@ namespace Battleships.Cmd
             return new Coordinate(row, column);
         }
 
-        private static void PrintCoordinateState(CoordinateState coordinateState)
-        {
-            switch (coordinateState)
-            {
-               case CoordinateState.Miss:
-                   Console.ForegroundColor = ConsoleColor.White;
-                   Console.Write(". ");
-                   Console.ResetColor();
-                   
-                   break;
-               case CoordinateState.Hit:
-                   Console.ForegroundColor = ConsoleColor.Yellow;
-                   Console.Write("x ");
-                   Console.ResetColor();
-                   
-                   break;
-               case CoordinateState.Sunk:
-                   Console.ForegroundColor = ConsoleColor.Red;
-                   Console.Write("x ");
-                   Console.ResetColor();
-                   
-                   break;
-               case CoordinateState.Unknown:
-                   Console.ResetColor();
-                   Console.Write("  ");
-
-                   break;
-            }
-        }
+      
         
         private static List<Coordinate> GetListOfAllFreeCoordinates()
         {
